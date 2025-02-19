5d7f6aacf662e8a49b69a51f496b323434f16b98
josedabh <josedabh@gmail.com>
2025-02-19 16:20:21 +0100

    Comparar cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { Error404Component } from './pages/error404/error404.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NoticiasListComponent } from './components/noticias/noticias-list/noticias-list.component';
 
 export const ROUTES = {
   NOTICIAS: 'noticias'
@@ -10,7 +11,11 @@ export const ROUTES = {
 export const Approutes: Routes = [
   {path: '' , redirectTo: ROUTES.NOTICIAS, pathMatch:'full'},
   {path: ROUTES.NOTICIAS, component : HomeComponent,
-    loadChildren: () => import("./pages/pages.routes").then(m => m.pagesRoutes)
+    children: [
+      { path: '', component: NoticiasListComponent,
+        loadChildren: () => import('./components/noticias/noticias.routes').then(m => m.noticiasRoutes),
+      }
+    ]
   },
   {path:'**', component: Error404Component}
 ];
--- src/app/pages/pages.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { NoticiasListComponent } from '../components/noticias/noticias-list/noticias-list.component';
-
-export const pagesRoutes: Routes = [
-  { path: '', component: NoticiasListComponent, 
-    loadChildren: () => import("../components/noticias/noticias.routes").then(m => m.noticiasRoutes)
-  },
-];
