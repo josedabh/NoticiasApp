f17570025279d3515a7a6e60e260325ceb1b3979
José Daniel Bravo Heredia <josedabh@gmail.com>
2025-02-20 08:00:41 +0100

    Arreglos

--- src/app/shared/services/noticias-api.service.ts
+++ src/app/shared/services/noticias-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ApiNews } from '../utils/data';
+import { ApiNews } from '../../utils/data';
+import { TOKEN } from '../../utils/token-api';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +9,17 @@ import { ApiNews } from '../utils/data';
 export class NoticiasApiService {
 
   readonly #http = inject(HttpClient);
+  readonly #tokenApi = inject(TOKEN);
 
   readonly #apiUrl = 'https://api.thenewsapi.com/v1/news/';
-  private apiToken = 'eELIqsCfVHICLUdWvyOf1cuTCs7GYHHM6ywEvzdb';
 
   getNews(limit: number = 3) {
-    const url = `${this.#apiUrl}/all?api_token=${this.apiToken}&language=es&limit=${limit}`;
+    const url = `${this.#apiUrl}/all?api_token=${this.#tokenApi}&language=es&limit=${limit}`;
     return this.#http.get<ApiNews>(url);
   }
 
   getNewsByUuid(uuid:string){
-    const url = `${this.#apiUrl}/uuid/${uuid}?api_token=${this.apiToken}`;
+    const url = `${this.#apiUrl}/uuid/${uuid}?api_token=${this.#tokenApi}`;
     return this.#http.get<ApiNews>(url);
   }
 }
